d393dd2c3ab075ff58c1aba4eb949a5ec9cb7dcf
PetarCetkovic <pcetkovic007@gmail.com>
2018-10-08 13:15:22 +0200

    v1.0 in progress

--- main.js
+++ main.js
@@ -54,3 +54,12 @@ window.onload = function() {
   responsiveSlider();  
 }
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
