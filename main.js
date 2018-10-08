3706928416bd4f89b3e9dc73da7ba910f24d4ec2
PetarCetkovic <pcetkovic007@gmail.com>
2018-10-08 15:17:12 +0200

    v1.1 bez responsiv menija

--- main.js
+++ main.js
@@ -64,24 +64,21 @@ function scrollFunction() {
     }
 }
 
-// Get the modal
 var modal1 = document.getElementById('myModal1');
 var modal2 = document.getElementById('myModal2');
 var modal3 = document.getElementById('myModal3');
 var modal4 = document.getElementById('myModal4');
 
-// Get the button that opens the modal
 var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
 var btn3 = document.getElementById('btn3');
 var btn4 = document.getElementById('btn4');
-// Get the <span> element that closes the modal
+
 var span1 = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close")[1];
 var span3 = document.getElementsByClassName("close")[2];
 var span4 = document.getElementsByClassName("close")[3];
 
-// When the user clicks on the button, open the modal 
 btn1.onclick = function() {
     modal1.style.display = "block";
 }
@@ -95,7 +92,6 @@ btn4.onclick = function() {
   modal4.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span2.onclick = function() {
     modal2.style.display = "none";
 }
@@ -110,7 +106,6 @@ span1.onclick = function() {
   modal1.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal1 || event.target == modal2 || event.target == modal3 || event.target == modal4) {
         modal1.style.display = "none";
